655e5a2e97bd92fe2bb92bbb248b18f607ad0633
pratz <pratz0909@gmail.com>
2018-03-21 23:38:01 +1100

    Render templates with golang

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 codedir='/usr/src/codetest'
 echo ">>> Compling code"
-docker run --rm -e GOPATH=/usr/ -v "$PWD":"$codedir" -w "$codedir" golang:1.8 sh -c "env && go get -v ./... && GOOS=linux go build -tags netgo -installsuffix netgo -v"
+docker run --rm -e GOPATH=/usr/ -v "$PWD":"$codedir" -w "$codedir" golang:1.10 sh -c "env && go get -v ./... && GOOS=linux go build -tags netgo -installsuffix netgo -v"
 echo ">>> Building docker container"
 docker build -t codetest .
--- main.go
+++ main.go
@@ -1,23 +1,42 @@
 // file mux_ex.go
 package main
 
+/*
+NOTE: Ideally templates should be rendered with Angular/React or something similar
+However, the coding challenge was to test golang skills, so rendering templates with golang itself.
+*/
+
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func baseHandler(w http.ResponseWriter, r *http.Request) {
+func index(w http.ResponseWriter, r *http.Request) {
+
+	// Read json content
 	fileContents, _ := ioutil.ReadFile("./json/response.json")
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Write(fileContents)
+	jsonContent := bytes.NewReader(fileContents)
+
+	// Unmarshal json
+	var data interface{}
+	if err := json.NewDecoder(jsonContent).Decode(&data); err != nil {
+		log.Println("Invalid json ", err)
+	}
+
+	// Render data in template
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	tmpl.Execute(w, data)
 }
 
 func main() {
 	r := mux.NewRouter()
-	log.Println("Waiting...")
-	r.HandleFunc("/", baseHandler)
+	r.HandleFunc("/", index)
+
+	log.Println("Starting server on localhost:80")
 	log.Fatal(http.ListenAndServe(":80", r))
 }
